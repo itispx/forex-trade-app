84fb5ec34f4d1876e1e62f516236c4f77722611f
urboypx <guilhermeciotapx@outlook.com>
2022-12-06 15:37:46 -0300

    tests: sign up modal

---
+++ apps/web-react/src/components/Modals/SignUpModal/SignUpModal.test.tsx
@@ -0,0 +1,43 @@
+import { screen, fireEvent } from "@testing-library/react";
+import { render } from "../../../utilities/testing";
+
+import SignUpModal from ".";
+
+describe("sign in modal component", () => {
+  describe("modal visibility", () => {
+    const handleClose = jest.fn();
+
+    it("should be visible", () => {
+      render(<SignUpModal show={true} close={handleClose} />);
+
+      const modal = screen.getByTestId("sign-up-modal");
+
+      expect(modal).toBeVisible();
+    });
+
+    it("should not visible", () => {
+      render(<SignUpModal show={false} close={handleClose} />);
+
+      const modal = screen.queryByTestId("sign-up-modal");
+
+      expect(modal).toBe(null);
+    });
+
+    it("should close modal", () => {
+      render(<SignUpModal show={true} close={handleClose} />);
+
+      const modal = screen.queryByTestId("sign-up-modal");
+
+      if (modal) {
+        fireEvent.keyDown(modal, {
+          key: "Escape",
+          code: "Escape",
+          keyCode: 27,
+          charCode: 27,
+        });
+      }
+
+      expect(handleClose).toHaveBeenCalledTimes(1);
+    });
+  });
+});
