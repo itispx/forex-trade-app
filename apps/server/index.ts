d0e1964e9aa4fc3451e5071eaeb0f0533c1aca37
urboypx <guilhermeciotapx@outlook.com>
2022-12-12 20:06:31 -0300

    chore: delete db connection on server start

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,16 +1,12 @@
 import * as http from "http";
 import app from "./src/app";
 
-import { connectDB } from "./src/util/db";
-
 const port = process.env.PORT || 3001;
 
 const server = http.createServer(app);
 
 server.listen(port, async () => {
   console.log(`Server running at http://localhost:${port}`);
-
-  await connectDB(process.env.MONGO_DB_URI as string);
 });
 
 import { Server } from "socket.io";
