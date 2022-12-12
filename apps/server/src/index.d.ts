519e360bce0efa551d3d22529ff037b2bf8eeec5
urboypx <guilhermeciotapx@outlook.com>
2022-12-12 09:54:12 -0300

    feat: update types in actions

--- apps/server/src/index.d.ts
+++ apps/server/src/index.d.ts
@@ -3,7 +3,7 @@ import { ObjectId } from "mongoose";
 declare global {
   namespace Express {
     export interface Request {
-      accessTokenID: ObjectId | undefined;
+      accessTokenID: string | undefined;
     }
   }
 }
