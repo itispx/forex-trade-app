8be6ff4a130d3532951116d3c43703fb0041f8a2
urboypx <guilhermeciotapx@outlook.com>
2022-12-12 18:29:40 -0300

    tests: user routes refactored

--- apps/server/src/util/testing.ts
+++ apps/server/src/util/testing.ts
@@ -1,37 +1,4 @@
-import request from "supertest";
-
-import { Express } from "express";
-
-import randomNumber from "./randomNumber";
-
-import { IUserServerResponse, IExchangeInfo, OCurrency, IUser } from "interfaces-common";
-
-export const createUserDB = async (app: Express): Promise<IUserServerResponse> => {
-  const response = await request(app).post("/v1/users/signup").send(userPayload);
-
-  return response.body.success;
-};
-
-export const makeExchangeInfoObj = (): IExchangeInfo => {
-  const baseCurrency = OCurrency[randomNumber(OCurrency.length - 1)];
-  const baseAmount = randomNumber(80, 1);
-
-  const convertCurrency = OCurrency.filter((cur) => baseCurrency !== cur)[
-    randomNumber(OCurrency.length - 2)
-  ];
-  const convertAmount = randomNumber(80, 1);
-
-  return {
-    base: { currency: baseCurrency, amount: baseAmount },
-    convert: { currency: convertCurrency, amount: convertAmount },
-  };
-};
-
-export const userPayload = {
-  _id: "",
-  username: "test_username",
-  password: "test_password",
-};
+import { IUserServerResponse, IUser } from "interfaces-common";
 
 export const userMock: IUser = {
   id: "01234567890",
