ec8b52ca40aa2d9b15106c2213ba8be2982bd3f9
urboypx <guilhermeciotapx@outlook.com>
2022-12-15 22:10:46 -0300

    fix: eslint ignore

--- apps/web/src/pages/Dashboard/index.tsx
+++ apps/web/src/pages/Dashboard/index.tsx
@@ -32,6 +32,7 @@ const DashboardPage: NextPage = () => {
     return () => {
       socket.disconnect();
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
