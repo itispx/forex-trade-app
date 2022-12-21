2e2e9595bc321dd323ee912573677d97b2ec2be8
urboypx <guilhermeciotapx@outlook.com>
2022-12-21 09:30:55 -0300

    fix: problems with running prisma

--- apps/server/src/util/errors/catchErrorHandler.ts
+++ apps/server/src/util/errors/catchErrorHandler.ts
@@ -3,6 +3,8 @@ import { Prisma } from "@prisma/client";
 import APIError from "./APIError";
 
 const catchErrorHandler = async (error: Error) => {
+  console.log("error:", error);
+
   if (error instanceof Prisma.PrismaClientKnownRequestError) {
     throw new APIError(error.code, error.message);
   }
